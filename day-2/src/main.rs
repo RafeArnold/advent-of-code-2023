81addc42068a1cd3c4cc68a515669b875db37cf2
rafearnold <rafe.arnold@gmail.com>
2023-12-02 10:29:20 +0000

    complete day 2 part 2.

--- day-2/src/main.rs
+++ day-2/src/main.rs
@@ -4,6 +4,7 @@ use std::collections::HashMap;
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
 fn run_1(input: &str) -> usize {
@@ -14,37 +15,64 @@ fn run_1(input: &str) -> usize {
         .sum()
 }
 
+fn run_2(input: &str) -> usize {
+    input.lines().map(power).sum()
+}
+
 fn is_possible(line: &str) -> bool {
-    // Strip prefix
-    let line = line.split_once(": ").unwrap().1;
-    let mut rounds = line.split("; ");
-    rounds.all(is_round_possible)
+    to_cubes(line).all(|(count, colour)| count <= *MAX_COUNTS.get(colour).unwrap())
 }
 
-fn is_round_possible(round: &str) -> bool {
-    let mut cubes = round.split(", ");
-    cubes.all(is_cube_count_possible)
-}
-
-fn is_cube_count_possible(cubes: &str) -> bool {
-    let (count, colour) = cubes.split_once(' ').unwrap();
-    count.parse::<u8>().unwrap() <= *MAX_COUNTS.get(colour).unwrap()
-}
-
-static MAX_COUNTS: Lazy<HashMap<&str, u8>> =
+static MAX_COUNTS: Lazy<HashMap<&str, usize>> =
     Lazy::new(|| HashMap::from([("red", 12), ("green", 13), ("blue", 14)]));
+
+fn power(line: &str) -> usize {
+    let counts = to_cubes(line).fold(
+        HashMap::<&str, usize>::with_capacity(3),
+        |mut acc, (count, colour)| {
+            acc.entry(colour)
+                .and_modify(|max| {
+                    if count > *max {
+                        *max = count
+                    }
+                })
+                .or_insert(count);
+            acc
+        },
+    );
+    counts.values().product()
+}
+
+fn to_cubes(line: &str) -> impl Iterator<Item = (usize, &str)> {
+    line.split_once(": ")
+        .unwrap()
+        .1
+        .split("; ")
+        .flat_map(|round| {
+            round.split(", ").map(|cubes| {
+                let (count, colour) = cubes.split_once(' ').unwrap();
+                (count.parse().unwrap(), colour)
+            })
+        })
+}
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn challenge_1() {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    static INPUT: &str = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
 Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        assert_eq!(run_1(input), 8);
+
+    #[test]
+    fn challenge_1() {
+        assert_eq!(run_1(INPUT), 8);
+    }
+
+    #[test]
+    fn challenge_2() {
+        assert_eq!(run_2(INPUT), 2286);
     }
 }
