d24ddf9f864faa0856e9b274a550e0565a77e651
rafearnold <rafe.arnold@gmail.com>
2023-12-10 21:40:22 +0000

    complete day 9 part 2.

--- day-9/src/main.rs
+++ day-9/src/main.rs
@@ -1,14 +1,42 @@
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
 fn run_1(input: &str) -> i32 {
-    let histories = parse_input(input);
-    histories.into_iter().map(next_value).sum()
+    run(
+        input,
+        &|values| values.last().copied(),
+        &|next_value, last_value| next_value + last_value,
+    )
 }
 
-fn next_value(history: Vec<i32>) -> i32 {
+fn run_2(input: &str) -> i32 {
+    run(
+        input,
+        &|values| values.first().copied(),
+        &|next_value, first_value| first_value - next_value,
+    )
+}
+
+fn run<E, O>(input: &str, extract: &E, op: &O) -> i32
+where
+    E: Fn(Vec<i32>) -> Option<i32>,
+    O: Fn(i32, i32) -> i32,
+{
+    let histories = parse_input(input);
+    histories
+        .into_iter()
+        .map(|history| next_value(history, extract, op))
+        .sum()
+}
+
+fn next_value<E, O>(history: Vec<i32>, extract: E, op: O) -> i32
+where
+    E: Fn(Vec<i32>) -> Option<i32>,
+    O: Fn(i32, i32) -> i32,
+{
     let mut sequences = Vec::new();
     sequences.push(history);
     loop {
@@ -23,8 +51,12 @@ fn next_value(history: Vec<i32>) -> i32 {
         sequences.push(next_sequence);
     }
     let mut next_value = 0;
-    for last_value in sequences.into_iter().map(|values| *values.last().unwrap()) {
-        next_value += last_value;
+    for value in sequences
+        .into_iter()
+        .map(|values| extract(values).unwrap())
+        .rev()
+    {
+        next_value = op(next_value, value);
     }
     next_value
 }
@@ -40,11 +72,17 @@ fn parse_input(input: &str) -> Vec<Vec<i32>> {
 mod tests {
     use super::*;
 
-    #[test]
-    fn challenge_1() {
-        const INPUT: &str = "0 3 6 9 12 15
+    const INPUT: &str = "0 3 6 9 12 15
 1 3 6 10 15 21
 10 13 16 21 30 45";
+
+    #[test]
+    fn challenge_1() {
         assert_eq!(run_1(INPUT), 114);
+    }
+
+    #[test]
+    fn challenge_2() {
+        assert_eq!(run_2(INPUT), 2);
     }
 }
