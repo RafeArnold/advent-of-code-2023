bc19db3d56cfde09cc3b0103881f0a6ecffa6258
rafearnold <rafe.arnold@gmail.com>
2023-12-22 11:44:29 +0000

    complete day 12 part 2.

--- day-12/src/main.rs
+++ day-12/src/main.rs
@@ -1,17 +1,41 @@
+use cached::proc_macro::cached;
+
 fn main() {
     const INPUT: &str = include_str!("../input.txt");
     println!("{}", run_1(INPUT));
+    println!("{}", run_2(INPUT));
 }
 
 fn run_1(input: &str) -> usize {
     parse_input(input).map(arrangements).sum()
 }
 
-fn arrangements((conditions, groups): (Vec<Condition>, Vec<u8>)) -> usize {
-    arrangements0(&conditions, &groups, None)
+fn run_2(input: &str) -> usize {
+    parse_input(input)
+        .map(|(conditions, groups)| {
+            (
+                conditions
+                    .repeat(5)
+                    .chunks_exact(conditions.len())
+                    .collect::<Vec<_>>()
+                    .join(&Condition::Unknown),
+                groups.repeat(5),
+            )
+        })
+        .map(arrangements)
+        .sum()
 }
 
-fn arrangements0(conditions: &[Condition], groups: &[u8], current_group_size: Option<u8>) -> usize {
+fn arrangements((conditions, groups): (Vec<Condition>, Vec<u8>)) -> usize {
+    arrangements0(conditions.to_vec(), groups.to_vec(), None)
+}
+
+#[cached]
+fn arrangements0(
+    conditions: Vec<Condition>,
+    groups: Vec<u8>,
+    current_group_size: Option<u8>,
+) -> usize {
     match conditions.get(0) {
         None => {
             if groups.is_empty() || (groups.len() == 1 && current_group_size == Some(groups[0])) {
@@ -29,11 +53,11 @@ fn arrangements0(conditions: &[Condition], groups: &[u8], current_group_size: Op
                     0
                 } else {
                     // We've reached the end of the current group.
-                    arrangements0(&conditions[1..], &groups[1..], None)
+                    arrangements0(conditions[1..].to_vec(), groups[1..].to_vec(), None)
                 }
             } else {
                 // Nothing else to check here. Just move on.
-                arrangements0(&conditions[1..], groups, None)
+                arrangements0(conditions[1..].to_vec(), groups, None)
             }
         }
         Some(Condition::Damaged) => {
@@ -43,34 +67,42 @@ fn arrangements0(conditions: &[Condition], groups: &[u8], current_group_size: Op
                     0
                 } else {
                     // Carry on moving through the current group.
-                    arrangements0(&conditions[1..], groups, Some(current_group_size + 1))
+                    arrangements0(
+                        conditions[1..].to_vec(),
+                        groups,
+                        Some(current_group_size + 1),
+                    )
                 }
             } else if groups.is_empty() {
                 // There are no more groups.
                 0
             } else {
                 // We've entered a new group.
-                arrangements0(&conditions[1..], groups, Some(1))
+                arrangements0(conditions[1..].to_vec(), groups, Some(1))
             }
         }
         Some(Condition::Unknown) => {
             if let Some(current_group_size) = current_group_size {
                 if groups[0] > current_group_size {
                     // The current group has not been filled yet, so the current spring has to be damaged.
-                    arrangements0(&conditions[1..], groups, Some(current_group_size + 1))
+                    arrangements0(
+                        conditions[1..].to_vec(),
+                        groups,
+                        Some(current_group_size + 1),
+                    )
                 } else {
                     // The current group has been filled, so the current spring has to be operational.
-                    arrangements0(&conditions[1..], &groups[1..], None)
+                    arrangements0(conditions[1..].to_vec(), groups[1..].to_vec(), None)
                 }
             } else {
                 // We are not in a group, so the current spring could be either operational or damaged (if there are remaining groups).
-                arrangements0(&conditions[1..], groups, None)
+                arrangements0(conditions[1..].to_vec(), groups.clone(), None)
                     + if groups.is_empty() {
                         // There are no more groups, so the spring cannot be damaged.
                         0
                     } else {
                         // There are more groups, so the spring could be damaged.
-                        arrangements0(&conditions[1..], groups, Some(1))
+                        arrangements0(conditions[1..].to_vec(), groups.to_vec(), Some(1))
                     }
             }
         }
@@ -96,7 +128,7 @@ fn parse_line(line: &str) -> (Vec<Condition>, Vec<u8>) {
     (conditions, groups)
 }
 
-#[derive(Debug)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum Condition {
     Operational,
     Damaged,
@@ -107,15 +139,21 @@ enum Condition {
 mod tests {
     use super::*;
 
-    #[test]
-    fn challenge_1() {
-        const INPUT: &str = "???.### 1,1,3
+    const INPUT: &str = "???.### 1,1,3
 .??..??...?##. 1,1,3
 ?#?#?#?#?#?#?#? 1,3,1,6
 ????.#...#... 4,1,1
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1";
+
+    #[test]
+    fn challenge_1() {
         assert_eq!(run_1(INPUT), 21);
+    }
+
+    #[test]
+    fn challenge_2() {
+        assert_eq!(run_2(INPUT), 525152);
     }
 
     #[test]
